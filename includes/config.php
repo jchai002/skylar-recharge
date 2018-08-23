a8200943f6a48cc950fbab9c926c49ee0dfbbdb9
Tim Nolan <j.tim.nolan@gmail.com>
2018-08-23 11:34:59 -0500

    Add commit file

---
+++ includes/config.php
@@ -0,0 +1,16 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+if (!function_exists('getallheaders')){ 
+    function getallheaders(){ 
+        $headers = []; 
+       foreach ($_SERVER as $name => $value){ 
+           if (substr($name, 0, 5) == 'HTTP_'){ 
+               $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value; 
+           } 
+       } 
+       return $headers; 
+    } 
+} 
