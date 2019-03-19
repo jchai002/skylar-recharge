e289d122f06cfa82064284d44e2634857588a80b
Tim <j.tim.nolan@gmail.com>
2019-03-19 13:24:47 -0500

    Add deploy script

--- git/deploy.php
+++ git/deploy.php
@@ -1,1 +1,10 @@
 <?php
+$commands = [
+	'echo $PWD',
+	'git pull',
+	'git status',
+];
+foreach($commands as $command){
+	$tmp = shell_exec("$command 2>&1");
+	echo $command.htmlentities(trim($tmp)) . "\n";
+}
