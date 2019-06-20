bf81edc4c9cd58290c35e34ead979f67a5dfb863
Tim <j.tim.nolan@gmail.com>
2019-06-20 12:04:29 -0700

    Deploy

---
+++ manual/sc_update_promo_intl_shipping.php
@@ -0,0 +1,22 @@
+<?php
+require_once(__DIR__.'/../includes/config.php');
+
+$rc = new RechargeClient();
+$sc = new ShopifyPrivateClient();
+
+$f = fopen(__DIR__.'/promos.csv', 'r');
+
+$headers = fgetcsv($f);
+
+$rownum = 0;
+while($row = fgetcsv($f)) {
+    $rownum++;
+    $row = array_combine($headers, $row);
+    print_r($row);
+    $row['order_id'] = $row['recharge shipping id'];
+    $row['address_id'] = $row['recharge purchase id'];
+    if(trim($row['country']) == 'United States'){
+        continue;
+    }
+
+}
