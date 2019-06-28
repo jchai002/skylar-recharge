9a91a660578f50c6fa006819dbd89f9eddab27f2
Tim Nolan <j.tim.nolan@gmail.com>
2019-06-28 16:35:49 -0700

    autocharge email progress

--- cron/send_autocharge_emails.php
+++ cron/send_autocharge_emails.php
@@ -9,11 +9,18 @@ require_once(__DIR__.'/../includes/config.php');
 // Send email
 
 $send_threshold = date('Y-m-d', strtotime('-5 days'));
-$stmt = $db->prepare("SELECT * FROM ac_orders aco
-LEFT JOIN order_line_items oli ON oli.id=ado.order_line_item_id
+$send_threshold = date('Y-m-d', strtotime('+5 days')); // TODO Remove
+$stmt = $db->prepare("SELECT aco.id, oli.sku, DATE(f.delivered_at) FROM ac_orders aco
+LEFT JOIN order_line_items oli ON oli.id=aco.order_line_item_id
 LEFT JOIN fulfillments f ON oli.fulfillment_id=f.id
-WHERE reminder_email_id IS NULL
-AND f.status='delivered'"); // TODO filters
+WHERE aco.reminder_email_id IS NULL
+AND f.shipment_status='delivered'
+AND f.delivered_at <= ?");
+
+$stmt->execute([$send_threshold]);
+
+print_r($stmt->fetchAll());
+die();
 
 $orders = $stmt->fetchAll();
 
@@ -21,6 +28,7 @@ $stmt = $db->prepare("UPDATE ac_orders SET reminder_email_id=:email_id WHERE id=
 foreach($orders as $ac_order){
     $res = klaviyo_send_transactional_email($db, $ac_order['email'], 'autocharge_choose_scent_reminder', [
         'first_name' => $ac_order['first_name'],
+        'sample_deliver_date' => $ac_order['delivered_at'],
     ]);
     if(!empty($res)){
         $stmt->execute([
--- test.php
+++ test.php
@@ -3,7 +3,9 @@ require_once(__DIR__.'/includes/config.php');
 
 $sc = new ShopifyClient();
 
-$res = $sc->get('/admin/orders/1217702199383/fulfillments.json');
+$res = $sc->get('/admin/orders/1089611956311/fulfillments.json');
+
+print_r($res);
 
 foreach($res as $fulfillment){
     echo insert_update_fulfillment($db, $fulfillment).PHP_EOL;
