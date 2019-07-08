15d90b86bf52e656fd50732c5fa108e465847cd7
Tim Nolan <j.tim.nolan@gmail.com>
2019-07-08 14:19:55 -0700

    remove hook debug code

--- git/deploy.php
+++ git/deploy.php
@@ -1,13 +1,13 @@
 <?php
 $hook_data = file_get_contents('php://input');
-echo $hook_data.PHP_EOL;
+//echo $hook_data.PHP_EOL;
 if(!empty($hook_data)){
 	$hook_data = json_decode($hook_data, true);
 }
 if(empty($hook_data)){
 	echo "Empty Payload ".json_last_error_msg();
 } else {
-	print_r($hook_data);
+//	print_r($hook_data);
 	if(strpos(getcwd(), 'production') !== false && $hook_data['ref'] != 'refs/heads/master'){
 		echo "Production / not master branch";
 		exit;
@@ -25,5 +25,5 @@ $commands = [
 ];
 foreach($commands as $command){
 	$tmp = shell_exec("$command 2>&1");
-	echo "$command ".htmlentities(trim($tmp)) . "\n";
+	echo "> $command ".PHP_EOL."< ".htmlentities(trim($tmp)) . "\n";
 }
