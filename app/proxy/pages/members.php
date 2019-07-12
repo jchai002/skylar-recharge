c38b26cc9af038739919c3869da668e1acf870d5
Tim Nolan <j.tim.nolan@gmail.com>
2019-07-12 14:34:54 -0700

    Add previous sc orders data

--- app/proxy/pages/members.php
+++ app/proxy/pages/members.php
@@ -9,8 +9,21 @@ foreach($recommended_products as $recommended_product){
 	$key = mt_rand(0,count($parts)-1);
 	$single_reco_products[] = $parts[$key];
 }
-$db->prepare("SELECT ")
+
+$stmt = $db->prepare("SELECT p.handle FROM orders o
+LEFT JOIN order_line_items oli ON oli.order_id=o.id
+LEFT JOIN variants v ON v.id=oli.variant_id
+LEFT JOIN products p ON p.id=v.product_id
+LEFT JOIN fulfillments f ON oli.fulfillment_id=f.id
+WHERE c.shopify_id = ?
+AND p.type = 'Scent Club Month'
+AND f.delivered_at IS NOT NULL
+GROUP BY handle;");
+$stmt->execute([$_REQUEST['c']]);
+$sc_received_handles = $stmt->fetchAll(PDO::FETCH_COLUMN);
+
 ?>
 {% assign portal_page = 'portal' %}
 {% assign recommended_product_handles = '<?=implode('|',$single_reco_products)?>' | split: '|' %}
+{% assign sc_received_handles = '<?=implode('|',$sc_received_handles)?>' | split: '|' %}
 {% include 'sc-member-portal' %}
--- manual/sync_order.php
+++ manual/sync_order.php
@@ -7,7 +7,7 @@ $sc = new ShopifyClient();
 
 $page_size = 250;
 $page = 0;
-/*
+
 do {
 	$page++;
 	$orders = $sc->get("/admin/orders.json", [
@@ -17,11 +17,15 @@ do {
 	]);
 
 	foreach($orders as $order){
-		echo insert_update_order($db, $order).PHP_EOL;
+		if($sc->callsLeft() < 5){
+			echo $sc->callsLeft()." calls remaining, taking a nap...".PHP_EOL;
+			sleep(5);
+		}
+		echo insert_update_order($db, $order, $sc)." ".$order['created_at'].PHP_EOL;
 	}
 } while(!empty($orders));
-*/
 
+/*
 $stmt = $db->query("SELECT o.shopify_id FROM orders o LEFT JOIN order_line_items oli ON o.id=oli.order_id
 WHERE o.created_at > '2019-03-01'
 AND oli.id IS NULL
@@ -33,3 +37,4 @@ foreach($stmt->fetchAll(PDO::FETCH_COLUMN) as $order_id){
 		echo insert_update_order($db, $order, $sc).PHP_EOL;
 	}
 }
+*/
