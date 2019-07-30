ed6dceebc495d0ed843db731724ef06c9c54ad78
Tim Nolan <j.tim.nolan@gmail.com>
2019-07-30 15:47:42 -0700

    update sync fulfillments

--- manual/sync_fulfillments.php
+++ manual/sync_fulfillments.php
@@ -12,11 +12,12 @@ do {
     $page++;
     $stmt = $db->query("SELECT DISTINCT o.shopify_id as shopify_order_id FROM order_line_items oli
 LEFT JOIN orders o ON o.id=oli.order_id
-WHERE oli.fulfillment_id IS NULL #LIMIT ".$page*$page_size.",".$page_size);
+LEFT JOIN fulfillments f ON f.id=oli.fulfillment_id
+WHERE f.tracking_number IS NULL #LIMIT ".$page*$page_size.",".$page_size);
     $order_ids = $stmt->fetchAll(PDO::FETCH_COLUMN);
     foreach($order_ids as $order_id){
         $res = $sc->get('/admin/orders/'.$order_id.'/fulfillments.json');
-        if($sc->callsLeft() < 5){
+        if($sc->callsLeft() < 10){
             sleep(1);
         }
         if(empty($res)){
