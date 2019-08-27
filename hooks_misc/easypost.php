61bd424ea1b4b9bd4357d69594faf9b010a36b7d
Tim Nolan <j.tim.nolan@gmail.com>
2019-08-27 10:35:59 -0700

    add request mode for easypost

--- hooks_misc/easypost.php
+++ hooks_misc/easypost.php
@@ -2,16 +2,23 @@
 http_response_code(200);
 require_once(__DIR__.'/../includes/config.php');
 
-$data = file_get_contents('php://input');
-$event = json_decode($data, true);
+if(!empty($_REQUEST['code'])){
+	$tracker = \EasyPost\Tracker::create([
+		'tracking_code' => $_REQUEST['code'],
+		'carrier' => $_REQUEST['code'],
+	]);
+} else {
+	$data = file_get_contents('php://input');
+	$event = json_decode($data, true);
 
-log_event($db, 'webhook', $event['id'], 'easypost_'.$event['result']['object'], $data);
+	log_event($db, 'webhook', $event['id'], 'easypost_'.$event['result']['object'], $data);
 
-if($event['result']['object'] != 'Tracker'){
-	die('Not a tracker');
+	if($event['result']['object'] != 'Tracker'){
+		die('Not a tracker');
+	}
+	$tracker = $event['result'];
 }
 
-$tracker = $event['result'];
 echo insert_update_tracker($db, $tracker);
 
 
