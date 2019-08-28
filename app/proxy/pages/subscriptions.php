5dde510a7f459d81b933d377714f22625d54276c
Tim Nolan <j.tim.nolan@gmail.com>
2019-08-28 16:06:13 -0700

    body account updates

--- app/proxy/pages/subscriptions.php
+++ app/proxy/pages/subscriptions.php
@@ -212,7 +212,7 @@ $shipment_list = $schedule->get()[0];
                 {% assign box_variant = svariant %}
                 {% endif %}
                 {% endfor %}
-                <div class="portal-subscription-item">
+                <div class="portal-item">
                     <div class="portal-item-edit">Edit</div>
                     <div class="portal-item-subscribed">Subscribed</div>
                     <div class="portal-item-details">
