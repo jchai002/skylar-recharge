7de565b0eb83b20a33e2c279a72915399a5107e9
Tim Nolan <j.tim.nolan@gmail.com>
2019-08-29 15:24:04 -0700

    body account updates

--- app/proxy/ajax/update_frequency.php
+++ app/proxy/ajax/update_frequency.php
@@ -3,34 +3,57 @@ header('Content-Type: application/json');
 
 global $db, $sc, $rc;
 
-if(empty($_REQUEST['subscription_id']) || empty($_REQUEST['variant_id'])){
+if(empty($_REQUEST['subscription_id']) || empty($_REQUEST['frequency'])){
 	die(json_encode([
 		'success' => false,
 		'errors' => [['msg'=>'Fields Missing']],
 	]));
 }
 
-$subscription_id = intval($_REQUEST['subscription_id']);
-$variant_id = intval($_REQUEST['variant_id']);
-
-$variant = get_variant($db, $variant_id);
-$subscription = get_rc_subscription($db, $subscription_id, $rc, $sc);
+$subscription = get_rc_subscription($db, intval($_REQUEST['subscription_id']), $rc, $sc);
+$frequency = $_REQUEST['frequency'];
 
 if($subscription['status'] == 'ONETIME'){
-	$res = $rc->put('/onetimes/'.$subscription_id, [
-		'product_title' => $variant['product_title'],
-		'shopify_variant_id' => $variant['shopify_id'],
-	]);
-	if(!empty($res['onetime'])){
-		insert_update_rc_subscription($db, $res['onetime'], $rc, $sc);
+	if($frequency != 'onetime'){
+		$rc->delete('/onetimes/'.$subscription['recharge_id']);
+		$res = $rc->post('/subscriptions', [
+			'address_id' => $subscription['recharge_address_id'],
+			'next_charge_scheduled_at' => $subscription['scheduled_at'],
+			'price' => $subscription['price'],
+			'quantity' => $subscription['quantity'],
+			'shopify_variant_id' => $subscription['shopify_variant_id'],
+			'product_title' => $subscription['product_title'],
+			'variant_title' => $subscription['variant_title'],
+			'order_interval_unit' => $subscription['order_interval_unit'],
+			'order_interval_frequency' => $frequency,
+			'charge_interval_frequency' => $subscription['charge_interval_frequency'],
+		]);
+		if(!empty($res['subscription'])){
+			insert_update_rc_subscription($db, $res['subscription'], $rc, $sc);
+		}
 	}
 } else {
-	$res = $rc->put('/subscriptions/'.$subscription_id, [
-		'product_title' => $variant['product_title'],
-		'shopify_variant_id' => $variant['shopify_id'],
-	]);
-	if(!empty($res['subscription'])){
-		insert_update_rc_subscription($db, $res['subscription'], $rc, $sc);
+	if($frequency != 'onetime'){
+		$res = $rc->put('/subscriptions/'.$subscription['recharge_id'], [
+			'order_interval_frequency' => $frequency,
+		]);
+		if(!empty($res['subscription'])){
+			insert_update_rc_subscription($db, $res['subscription'], $rc, $sc);
+		}
+	} else {
+		$rc->delete('/subscriptions/'.$subscription['recharge_id']);
+		$res = $rc->post('/onetimes', [
+			'address_id' => $subscription['recharge_address_id'],
+			'next_charge_scheduled_at' => $subscription['scheduled_at'],
+			'price' => $subscription['price'],
+			'quantity' => $subscription['quantity'],
+			'shopify_variant_id' => $subscription['shopify_variant_id'],
+			'product_title' => $subscription['product_title'],
+			'variant_title' => $subscription['variant_title'],
+		]);
+		if(!empty($res['onetime'])){
+			insert_update_rc_subscription($db, $res['onetime'], $rc, $sc);
+		}
 	}
 }
 
