b06195f097fc6e64f6086cdcf32b89c23e0d70d9
Tim Nolan <j.tim.nolan@gmail.com>
2019-08-30 10:36:00 -0700

    Update sc untag to only do so on the right day

--- cron/sc_untag_blackout_db.php
+++ cron/sc_untag_blackout_db.php
@@ -1,6 +1,12 @@
 <?php
 require_once(__DIR__.'/../includes/config.php');
 
+$first_of_current_month = date('Y-m-01');
+$untag_time = offset_date_skip_weekend(strtotime($first_of_current_month));
+if(date('Y-m-d') != date('Y-m-d', $untag_time)){
+	die('Today is not the day to untag!');
+}
+
 $sc = new ShopifyClient();
 
 $stmt = $db->query("SELECT shopify_id as id, tags FROM orders WHERE tags LIKE '%HOLD: Scent Club Blackout%'");
