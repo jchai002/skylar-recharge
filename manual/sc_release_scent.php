14224cf87f0132f7e925b2f237cc79e8485a9c7b
Tim Nolan <j.tim.nolan@gmail.com>
2019-08-30 10:45:20 -0700

    add estimated finish time to sc_release

--- manual/sc_release_scent.php
+++ manual/sc_release_scent.php
@@ -52,7 +52,9 @@ foreach($charges as $index=>$charge){
     }
 	echo sc_calculate_next_charge_date($db, $rc, $charge['address_id']).PHP_EOL;
 	if($index % 20 == 0){
-		echo "Updated: ".$index."/".count($charges)." Rate: ".($index / (microtime(true) - $start_time))." charges/s".PHP_EOL;
+		$charges_per_sec = ($index / (microtime(true) - $start_time));
+		$time = time() + (count($charges)-$index)*($index / (microtime(true) - $start_time));
+		echo "Updated: ".$index."/".count($charges)." Rate: ".$charges_per_sec." charges/s, Estimated finish: ".date('H:i:s',$time).PHP_EOL;
 	}
 }
 
