1033b76b7bb39527b826e8aa14cb02996c08927c
Tim Nolan <j.tim.nolan@gmail.com>
2019-08-29 22:56:01 -0700

    body account updates

--- app/proxy/pages/subscriptions.php
+++ app/proxy/pages/subscriptions.php
@@ -486,6 +486,9 @@ $shipment_list = $schedule->get()[0];
                 LEFT JOIN variants v ON va.variant_id=v.id
                 WHERE va.format_id=:format_id AND va.product_type_id=:product_type_id;");
 			foreach($schedule->onetimes() as $item){
+			    if(!empty(get_oli_attribute($item, '_parent_id')) && !in_array(get_oli_attribute($item, '_parent_id'), $schedule->subscriptions())){
+			        continue;
+                }
 				echo "<!--";
 				// TODO SC will sometimes be a onetime
 				$variant = get_variant($db, $item['shopify_variant_id']);
