377b97b2c2df28408d7f17b5ad4442277cdc3efe
Tim Nolan <j.tim.nolan@gmail.com>
2019-09-04 16:12:25 -0700

    body account update

--- app/proxy/pages/subscriptions.php
+++ app/proxy/pages/subscriptions.php
@@ -803,7 +803,6 @@ $shipment_list = $schedule->get()[0];
                 afterOpen: $.noop, // Fix dumb app bug
             });
         });
-        debugger;
         optional_scripts.onload('pignose', function(){
             $('.portal-item-edit-container .calendar').each(function(){
                 var box = $(this).closest('.portal-item');
