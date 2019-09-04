fe9e97dc32e66c97c55e3a577a25ae8ed232c247
Tim Nolan <j.tim.nolan@gmail.com>
2019-09-03 18:06:44 -0700

    fix bind_Events calendar

--- app/proxy/pages/subscriptions.php
+++ app/proxy/pages/subscriptions.php
@@ -1009,7 +1009,22 @@ $shipment_list = $schedule->get()[0];
                 afterOpen: $.noop, // Fix dumb app bug
             });
         });
-        optional_scripts.onload('pignose', function(){
+        if(!optional_scripts.resources.pignose.loaded){
+            optional_scripts.onload('pignose', function(){
+                $('.portal-item-edit-container .calendar').each(function(){
+                    var box = $(this).closest('.portal-item');
+                    $(this).pignoseCalendar({
+                        date: moment(parseInt(box.data('ship-time'))*1000),
+                        minDate: moment(),
+                        disabledWeekdays: [0, 6],
+                        select: function(date){
+                            AccountController.update_subscription_date(box.data('subscription-id'), date[0].format('YYYY-MM-DD'));
+                            box.find('.calendar').slideUp();
+                        },
+                    });
+                });
+            });
+        } else {
             $('.portal-item-edit-container .calendar').each(function(){
                 var box = $(this).closest('.portal-item');
                 $(this).pignoseCalendar({
@@ -1022,7 +1037,7 @@ $shipment_list = $schedule->get()[0];
                     },
                 });
             });
-        });
+        }
 
         $('.remove-discount-link').unbind().click(function(e){
             e.preventDefault();
