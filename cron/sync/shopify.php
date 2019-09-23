cd13774ef7df9138ea054ba199af2b2f14893ac5
Tim Nolan <j.tim.nolan@gmail.com>
2019-09-23 15:52:55 -0700

    Add ac daily sync to shopify sync

--- cron/sync/shopify.php
+++ cron/sync/shopify.php
@@ -5,6 +5,7 @@ $interval = 5;
 $page_size = 250;
 $sc = new ShopifyClient();
 $min_date = date('Y-m-d H:i:00P', time()-60*6);
+$start_time = time();
 
 // Products
 echo "Updating products".PHP_EOL;
@@ -58,3 +59,30 @@ do {
 		}
 	}
 } while(count($res) >= $page_size);
+
+
+// Daily syncs
+if(
+	(date('G', $start_time) == 12 && date('i', $start_time) < 4 && !in_array(date('N', $start_time), [6,7]))
+	|| (!empty($argv) && !empty($argv[1]) && $argv[1] == 'all')
+){
+	echo "Updating missing AC fulfillments".PHP_EOL;
+	$stmt = $db->query("SELECT o.shopify_id FROM ac_orders aco
+		LEFT JOIN order_line_items oli ON aco.order_line_item_id=oli.id
+		LEFT JOIN fulfillments f ON f.id=oli.fulfillment_id
+		LEFT JOIN orders o ON oli.order_id=o.id
+		WHERE oli.fulfillment_id IS NULL
+		AND o.created_at < '".date('Y-m-d', $start_time - (24*60*60))."'
+		AND o.cancelled_at IS NULL;");
+	foreach($stmt->fetchAll(PDO::FETCH_COLUMN) as $order_id){
+		echo " - ".$order_id.": ".PHP_EOL;
+		$fulfillment_res = $sc->get('/admin/orders/'.$order_id.'/fulfillments.json', [
+			'updated_at_min' => $min_date,
+			'limit' => $page_size,
+			'page' => $page,
+		]);
+		foreach($fulfillment_res as $fulfillment){
+			echo "   - ".insert_update_fulfillment($db, $fulfillment).PHP_EOL;
+		}
+	}
+}
