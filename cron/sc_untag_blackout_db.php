181efd7246fb782a3bb7ae661340a4450bd7d38f
Tim Nolan <j.tim.nolan@gmail.com>
2019-10-08 14:29:25 -0700

    add additional sync to untag blackout script

--- cron/sc_untag_blackout_db.php
+++ cron/sc_untag_blackout_db.php
@@ -3,11 +3,31 @@ require_once(__DIR__.'/../includes/config.php');
 
 $first_of_current_month = date('Y-m-01');
 $untag_time = offset_date_skip_weekend(strtotime($first_of_current_month));
-if(date('Y-m-d') != date('Y-m-d', $untag_time)){
+if(date('Y-m-d') != date('Y-m-d', $untag_time) && (empty($argv[1]) || $argv[1] != 'force')){
 	die('Today is not the day to untag!');
 }
 
-$sc = new ShopifyClient();
+$today = date('Y-m-d');
+// First, make sure we are fully synced
+$page = 0;
+$page_size = 250;
+$total_orders = 0;
+$start_time = time();
+do {
+	$page++;
+	$orders = $sc->get('/admin/orders.json', [
+		'created_at_min' => $today,
+		'limit' => $page_size,
+		'page' => $page,
+		'order' => 'created_at asc',
+	]);
+	foreach($orders as $order){
+		$total_orders++;
+		echo insert_update_order($db, $order, $sc).PHP_EOL;
+	}
+	$elapsed_time = (time()-$start_time)/60;
+	echo "Synced $total_orders in ".round($elapsed_time, 2)."m ".round($total_orders/($elapsed_time), 2)." orders/min".PHP_EOL;
+} while(count($orders) >= $page_size);
 
 $stmt = $db->query("SELECT shopify_id as id, tags FROM orders WHERE tags LIKE '%HOLD: Scent Club Blackout%'");
 
