a3d419870eb8fa5b9bcfaf0d74689e151eb05f58
Tim Nolan <j.tim.nolan@gmail.com>
2019-10-14 17:24:43 -0700

    add multiple variants to conver old subscription script

--- manual/convert_old_subscriptions.php
+++ manual/convert_old_subscriptions.php
@@ -1,32 +1,43 @@
 <?php
 require_once(__DIR__.'/../includes/config.php');
 
-// Load all active subscriptions
-$subscriptions = [];
-$page_size = 250;
-$page = 0;
-do {
-	$page++;
-	$res = $rc->get('/subscriptions', [
-		'status' => 'ACTIVE',
-		'limit' => $page_size,
-		'page' => $page,
-		'shopify_variant_id' => 19519443370071,
-	]);
-	foreach($res['subscriptions'] as $subscription){
-		// Sort them by address id
-		$subscriptions[] = $subscription;
-	}
-} while(count($res) >= $page_size);
+$convert_variant_ids = [
+	12235409129559 => 30258959482967,
+	12235492425815 => 30258951389271,
+	12235492360279 => 30258952175703,
+	12235492327511 => 30258950996055,
+	12235492393047 => 30258958958679,
+	12588614484055 => 30258961973335,
+];
 
-foreach($subscriptions as $subscription){
-	echo "Updating ".$subscription['id'].PHP_EOL;
-	$res = $rc->put('/subscriptions/'.$subscription['id'], [
-		'shopify_variant_id' => 19787922014295,
-	]);
-	if(empty($res['subscription'])){
-		echo "Error ".print_r($res);
-		die();
+foreach($convert_variant_ids as $old_variant_id => $new_variant_id){
+	// Load all active subscriptions
+	$subscriptions = [];
+	$page_size = 250;
+	$page = 0;
+	do {
+		$page++;
+		$res = $rc->get('/subscriptions', [
+			'status' => 'ACTIVE',
+			'limit' => $page_size,
+			'page' => $page,
+			'shopify_variant_id' => $old_variant_id,
+		]);
+		foreach($res['subscriptions'] as $subscription){
+			// Sort them by address id
+			$subscriptions[] = $subscription;
+		}
+	} while(count($res) >= $page_size);
+
+	foreach($subscriptions as $subscription){
+		echo "Updating ".$subscription['id'].PHP_EOL;
+		$res = $rc->put('/subscriptions/'.$subscription['id'], [
+			'shopify_variant_id' => $new_variant_id,
+		]);
+		if(empty($res['subscription'])){
+			echo "Error ".print_r($res);
+			die();
+		}
+		insert_update_rc_subscription($db, $res['subscription'], $rc, $sc);
 	}
-	insert_update_rc_subscription($db, $res['subscription'], $rc, $sc);
 }
