09f17bd40f81a3f59e0ee52585138d80b2822790
Tim Nolan <j.tim.nolan@gmail.com>
2019-10-17 13:11:20 -0700

    Change promo sku script to use api, not export

--- manual/sc_update_promo_sku.php
+++ manual/sc_update_promo_sku.php
@@ -1,14 +1,58 @@
 <?php
 require_once(__DIR__.'/../includes/config.php');
 
-$rc = new RechargeClient();
-$sc = new ShopifyClient();
+$new_sku = '10213908-116';
+
+$page = 0;
+$orders = [];
+do {
+	$page++;
+	$res = $rc->get('/orders', [
+		'page' => $page,
+		'limit' => 250,
+		'status' => 'queued',
+		'scheduled_at' => date('Y-m-19'),
+	]);
+	foreach($res['orders'] as $order){
+		if(count($order['line_items']) != 1){
+			echo "Not 1 line item!";
+			die();
+		}
+		if($order['line_items'][0]['shopify_variant_id'] != 28003712663639){
+			echo "Skipping order because not a promo ".$order['id']." ".$order['email'].PHP_EOL;
+			die();
+		}
+		$orders[] = $order;
+	}
+} while(count($res['orders']) >= 250);
+
+foreach($orders as $order){
+	$line_item = [
+		'sku' => $new_sku,
+		'price' => $order['line_items'][0]['price'],
+		'properties' => $order['line_items'][0]['properties'],
+		'quantity' => $order['line_items'][0]['quantity'],
+		'subscription_id' => $order['line_items'][0]['subscription_id'],
+		'title' => $order['line_items'][0]['title'],
+		'product_title' => $order['line_items'][0]['product_title'],
+		'variant_title' => $order['line_items'][0]['variant_title'],
+		'product_id' => $order['line_items'][0]['shopify_product_id'],
+		'variant_id' => $order['line_items'][0]['shopify_variant_id'],
+	];
+	$res = $rc->put('/orders/'.$order['id'], ['line_items' => [$line_item]]);
+	echo "Updated ".$order['id']." ".$order['email'].PHP_EOL;
+	if(empty($res['order']) || $res['order']['line_items'][0]['sku'] != $new_sku){
+		print_r($res);
+		echo "ERROR CANNOT UPDATE ORDER";
+		die();
+	}
+}
+
+die();
+
 
 $f = fopen(__DIR__.'/upcoming_promos.csv', 'r');
-
 $headers = fgetcsv($f);
-
-$new_sku = '10213906-114';
 
 $rownum = 0;
 while($row = fgetcsv($f)) {
