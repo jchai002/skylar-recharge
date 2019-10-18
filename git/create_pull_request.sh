c0b97d7865be3644248bb06533c63133165beb52
Tim Nolan <j.tim.nolan@gmail.com>
2019-10-18 10:28:06 -0700

    add pullme theme process

---
+++ git/create_pull_request.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+whoami
+cd ~/repos/$1/skylar-shopify-theme
+echo $PWD
+git checkout master
+git pull
+git checkout settings-theme-$2 || git checkout -b settings-theme-$2
+echo $4 > src/config/settings_data.json
+git commit -am "pull settings from shopify" && git push
+[[ -z $(hub pr list -h settings-theme-$2) ]] && hub pull-request -m "Settings update: "$3
--- hooks/theme_updated.php
+++ hooks/theme_updated.php
@@ -16,40 +16,12 @@ echo insert_update_theme($db, $theme);
 
 if(strpos(strtolower($theme['name']), '[pullme]') !== 'false'){
 	$settings_data = $sc->get('/admin/api/2019-10/themes/'.$theme['id'].'/assets.json', ['asset'=>['key'=>'config/settings_data.json']])['value'];
-
-	die();
 	$dir = ENV_TYPE == 'LIVE' ? 'production' : 'staging';
-	$commands = [
-		'whoami',
-		'su -u deploy',
-		'cd ~/repos/'.$dir.'/skylar-shopify-theme',
-		'echo $PWD',
-//		'git checkout master',
-//		'sudo -u deploy /usr/bin/git pull',
-//		'git checkout settings-theme-'.$theme['id'].' || git checkout -b settings-theme-'.$theme['id'],
-	];
-	foreach($commands as $command){
-		$tmp = shell_exec("$command 2>&1");
-		echo "> $command ".PHP_EOL."< ".htmlentities(trim($tmp)) . "\n";
-	}
-	file_put_contents('/home/deploy/repos/'.$dir.'/src/config/settings_data.json', $settings_data);
-	$command = 'git commit -am "pull settings from shopify" && git push';
+
+	$command = "sudo -u deploy ../git/create_pull_request.sh $dir ".$theme['id'].' "'.addcslashes(trim(str_replace('[pullme]', '', $theme['name'])), '"').'" "'.addcslashes($settings_data, '"').'"';
+
 	$tmp = shell_exec("$command 2>&1");
 	echo "> $command ".PHP_EOL."< ".htmlentities(trim($tmp)) . "\n";
-
-	$command = "GITHUB_TOKEN=".$_ENV['GITHUB_TOKEN'];
-	$tmp = shell_exec("$command 2>&1");
-	echo "> ".str_replace($_ENV['GITHUB_TOKEN'], '***', $command)." ".PHP_EOL."< ".htmlentities(trim(str_replace($_ENV['GITHUB_TOKEN'], '***', $tmp))) . "\n";
-
-	$command = 'hub pr list -h settings-theme-'.$theme['id'];
-	$tmp = shell_exec("$command 2>&1");
-	echo "> $command ".PHP_EOL."< ".htmlentities(trim($tmp)) . "\n";
-	if(empty(trim($tmp))){
-		// Create pull request
-		$command = 'hub pull-request -m "Settings update: '.trim(str_replace('[pullme]', '', $theme['name'])).'"';
-		$tmp = shell_exec("$command 2>&1");
-		echo "> $command ".PHP_EOL."< ".htmlentities(trim($tmp)) . "\n";
-	}
 
 	// TODO Get PR number and update theme name
 
