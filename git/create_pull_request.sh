ddeb1e64e12de0389866aea738956b65a7ad2a3c
Tim Nolan <j.tim.nolan@gmail.com>
2019-10-18 13:19:34 -0700

    add pullme theme process

--- git/create_pull_request.sh
+++ git/create_pull_request.sh
@@ -8,7 +8,11 @@ git checkout settings-theme-$2 && git pull || git checkout -b settings-theme-$2
 echo $5 > src/config/settings_data.json
 hub pr list -h settings-theme-$2
 git commit -am "pull settings from shopify" && git push --set-upstream origin settings-theme-$2
-if [[ $(curl -u JTimNolan:$3 -X GET "https://api.github.com/repos/JTimNolan/skylar-shopify-theme/pulls?head=skylar-shopify-theme:settings-theme-$2" | jq -r '.') == "[]" ]]
+PULL_REQUEST=$(curl -u JTimNolan:$3 -X GET "https://api.github.com/repos/JTimNolan/skylar-shopify-theme/pulls?head=skylar-shopify-theme:settings-theme-$2" | jq -r '.')
+if [[ PULL_REQUEST == "[]" ]]
 then
-    echo $(curl -g -u JTimNolan:$3 -H "Content-Type: application/json" -d '{"title":"Settings update: '$4'", "head":"settings-theme-'$2'", "base":"master"}' -X POST "https://api.github.com/repos/JTimNolan/skylar-shopify-theme/pulls" | jq -r '.')
+    PULL_REQUEST=$(curl -g -u JTimNolan:$3 -H "Content-Type: application/json" -d '{"title":"Settings update: '$4'", "head":"settings-theme-'$2'", "base":"master"}' -X POST "https://api.github.com/repos/JTimNolan/skylar-shopify-theme/pulls" | jq -r '.')
+else
+    PULL_REQUEST=$(PULL_REQUEST | jq -r '.[0]')
 fi
+echo PULL_REQUEST
