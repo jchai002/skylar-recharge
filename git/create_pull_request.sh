b7c75a25ee3a0bd59366f05b4ba291a8392009c2
Tim Nolan <j.tim.nolan@gmail.com>
2019-10-18 12:02:44 -0700

    add pullme theme process

--- git/create_pull_request.sh
+++ git/create_pull_request.sh
@@ -5,6 +5,8 @@ cd /home/deploy/repos/$1/skylar-shopify-theme
 echo $PWD
 git checkout master
 git pull
-git checkout settings-theme-$2 || git checkout -b settings-theme-$2
+git checkout settings-theme-$2 && git pull || git checkout -b settings-theme-$2
 echo $4 > src/config/settings_data.json
-git commit -am "pull settings from shopify" && git push --set-upstream origin settings-theme-$2 && [[ -z $(hub pr list -h settings-theme-$2) ]] && hub pull-request -m "Settings update: "$3
+hub pr list -h settings-theme-$2
+git commit -am "pull settings from shopify" && git push --set-upstream origin settings-theme-$2
+[[ -z $(hub pr list -h settings-theme-$2) ]] && hub pull-request -m "Settings update: "$3
