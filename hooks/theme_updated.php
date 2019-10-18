4a415aba25cafcec1d0d5c7c95d32483d4afa4e0
Tim Nolan <j.tim.nolan@gmail.com>
2019-10-18 10:29:52 -0700

    add pullme theme process

--- hooks/theme_updated.php
+++ hooks/theme_updated.php
@@ -17,6 +17,7 @@ echo insert_update_theme($db, $theme);
 if(strpos(strtolower($theme['name']), '[pullme]') !== 'false'){
 	$settings_data = $sc->get('/admin/api/2019-10/themes/'.$theme['id'].'/assets.json', ['asset'=>['key'=>'config/settings_data.json']])['value'];
 	$dir = ENV_TYPE == 'LIVE' ? 'production' : 'staging';
+	$settings_data = 'test';
 
 	$command = "sudo -u deploy ../git/create_pull_request.sh $dir ".$theme['id'].' "'.addcslashes(trim(str_replace('[pullme]', '', $theme['name'])), '"').'" "'.addcslashes($settings_data, '"').'"';
 
