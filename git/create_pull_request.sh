786cbb940a0915a9381b368ec27b46306cef15ff
Tim Nolan <j.tim.nolan@gmail.com>
2019-10-18 11:29:54 -0700

    add pullme theme process

--- git/create_pull_request.sh
+++ git/create_pull_request.sh
@@ -7,6 +7,5 @@ git checkout master
 git pull
 git checkout settings-theme-$2 || git checkout -b settings-theme-$2
 echo $4 > src/config/settings_data.json
-exit
 git commit -am "pull settings from shopify" && git push
 [[ -z $(hub pr list -h settings-theme-$2) ]] && hub pull-request -m "Settings update: "$3
