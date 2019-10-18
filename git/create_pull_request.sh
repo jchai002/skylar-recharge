bdf6ea27b2f39a52a6939a26b9e57460e5ed8233
Tim Nolan <j.tim.nolan@gmail.com>
2019-10-18 10:56:33 -0700

    add pullme theme process

--- git/create_pull_request.sh
+++ git/create_pull_request.sh
@@ -4,8 +4,8 @@ whoami
 cd /home/deploy/repos/$1/skylar-shopify-theme
 echo $PWD
 git checkout master
-exit
 git pull
+exit
 git checkout settings-theme-$2 || git checkout -b settings-theme-$2
 echo $4 > src/config/settings_data.json
 git commit -am "pull settings from shopify" && git push
