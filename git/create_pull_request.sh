1538cd8d5582a4c178d6296d3aa490717e3ad0f4
Tim Nolan <j.tim.nolan@gmail.com>
2019-10-18 12:58:50 -0700

    add pullme theme process

--- git/create_pull_request.sh
+++ git/create_pull_request.sh
@@ -10,6 +10,7 @@ hub pr list -h settings-theme-$2
 git commit -am "pull settings from shopify" && git push --set-upstream origin settings-theme-$2
 if [[ $(curl -u JTimNolan:$3 -X GET "https://api.github.com/repos/JTimNolan/skylar-shopify-theme/pulls?head=skylar-shopify-theme:settings-theme-$2" | jq -r '.') == "[]" ]]
 then
-    echo "curl -u JTimNolan:$3 -H \"Content-Type: application/json\" -d '{\"title\":\"Settings update: '"$4"'\", \"head\":\"skylar-shopify-theme:settings-theme-'"$2"'\", \"base\":\"master\"}' -X POST \"https://api.github.com/repos/JTimNolan/skylar-shopify-theme/pulls\" | jq -r '.'"
-    echo $(curl -u JTimNolan:$3 -H "Content-Type: application/json" -d '{"title":"Settings update: '$4'", "head":"skylar-shopify-theme:settings-theme-'$2'", "base":"master"}' -X POST "https://api.github.com/repos/JTimNolan/skylar-shopify-theme/pulls" | jq -r '.')
+    echo "curl -u JTimNolan:\$GITHUB_TOKEN -H \"Content-Type: application/json\" -d '{\"title\":\"Settings update: '"$4"'\", \"head\":\"skylar-shopify-theme:settings-theme-'"$2"'\", \"base\":\"master\"}' -X POST \"https://api.github.com/repos/JTimNolan/skylar-shopify-theme/pulls\" | jq -r '.'"
+
+    echo $(curl -u JTimNolan:$3 -H "Content-Type: application/json" -d "{\"title\":\"Settings update: "$4"\", \"head\":\"skylar-shopify-theme:settings-theme-"$2"\", \"base\":\"master\"}" -X POST "https://api.github.com/repos/JTimNolan/skylar-shopify-theme/pulls" | jq -r '.')
 fi
