ddac066115e0d57de214617c166d27ecae7a924f
Tim Nolan <j.tim.nolan@gmail.com>
2019-10-18 16:04:14 -0700

    add pullme theme process

--- git/prep_theme_commit.sh
+++ git/prep_theme_commit.sh
@@ -4,4 +4,4 @@ cd /home/deploy/repos/$1/skylar-shopify-theme || exit 1
 echo $PWD
 git checkout master
 git pull
-git checkout settings-theme-$2 && git pull || git checkout -b settings-theme-$2
+git checkout settings-theme-$2 && git pull || git checkout -b settings-theme-$2 --set-upstream origin settings-theme-$2
