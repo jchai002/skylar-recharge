24c5b396654eb28a72cb1760cf2dd86c74ccccb2
Tim Nolan <j.tim.nolan@gmail.com>
2019-10-20 15:45:31 -0700

    add pullme theme process

--- git/make_theme_commit.sh
+++ git/make_theme_commit.sh
@@ -5,5 +5,5 @@ echo $PWD
 git checkout master
 git pull
 git checkout settings-theme-$2 && git pull || git checkout -b settings-theme-$2 && git branch --set-upstream-to=origin settings-theme-$2
-curl -g -X GET "https://"$3"@maven-and-muse.myshopify.com/admin/api/2019-10/themes/"$2"/assets.json?asset[key]=config/settings_data.json" | jq '.asset.value' > src/config/settings_data.json
+curl -g -X GET "https://"$3"@maven-and-muse.myshopify.com/admin/api/2019-10/themes/"$2"/assets.json?asset[key]=config/settings_data.json" | jq -r '.asset.value' > src/config/settings_data.json
 git commit -am "pull settings from shopify" && git push --set-upstream origin settings-theme-$2
