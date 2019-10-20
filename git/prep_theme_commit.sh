825819e391af2bd51eb23f6630de46fa0a9af840
Tim Nolan <j.tim.nolan@gmail.com>
2019-10-20 15:23:08 -0700

    add pullme theme process

--- git/prep_theme_commit.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-cd /home/deploy/repos/$1/skylar-shopify-theme || exit 1
-echo $PWD
-git checkout master
-git pull
-git checkout settings-theme-$2 && git pull || git checkout -b settings-theme-$2 && git branch --set-upstream-to=origin settings-theme-$2
-curl -X GET "https://"$3"@maven-and-muse.myshopify.com/admin/api/2019-10/themes/"$2"/assets.json?asset[key]=config/settings_data.json" | jq '.value' > src/config/settings_data.json
-git commit -am "pull settings from shopify" && git push --set-upstream origin settings-theme-$2
