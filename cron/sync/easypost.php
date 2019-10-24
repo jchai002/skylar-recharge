6507f2b0411b2f50e6736df9787f7d44d2427736
Tim Nolan <j.tim.nolan@gmail.com>
2019-10-24 13:35:50 -0700

    add backstop to easypost sync

--- cron/sync/easypost.php
+++ cron/sync/easypost.php
@@ -4,8 +4,14 @@ require_once(__DIR__.'/../../includes/config.php');
 // Check in_transit shipments from before today
 
 $date = date('Y-m-d');
+$this_hour = date('H') - 1;
+$this_hour = $this_hour < 0 ? 0 : $this_hour;
 
-$stmt = $db->query("SELECT * FROM skylar.ep_trackers WHERE status NOT IN ('delivered', 'failure', 'return_to_sender') AND created_at < '$date' AND updated_at < '$date';");
+$max_date = date('Y-m-d', strtotime('-60 days'));
+
+$stmt = $db->query("SELECT * FROM skylar.ep_trackers WHERE status NOT IN ('delivered', 'failure', 'return_to_sender') AND created_at BETWEEN '$max_date' AND '$date' AND updated_at < '$date $this_hour:30:00';");
+
+echo $stmt->queryString.PHP_EOL;
 
 foreach($stmt->fetchAll() as $row){
 	$tracker = \EasyPost\Tracker::create([
