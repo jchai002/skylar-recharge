319e864159cd4ca62ba9eb79b280de422164e2e3
Tim Nolan <j.tim.nolan@gmail.com>
2019-10-28 11:41:29 -0700

    add product debug output

--- app/proxy/pages/addtobox_lander.php
+++ app/proxy/pages/addtobox_lander.php
@@ -45,7 +45,7 @@ if(!empty($add_to_charge)){
 	$month = date('F', strtotime($add_to_charge['scheduled_at']));
 
     // Check if they already have this product in a sub
-        $stmt = $db->prepare("SELECT * FROM rc_subscriptions rcs
+    $stmt = $db->prepare("SELECT * FROM rc_subscriptions rcs
     LEFT JOIN rc_addresses rca ON rcs.address_id=rca.id
     LEFT JOIN rc_customers rcc ON rca.rc_customer_id=rcc.id
     WHERE (rcs.status = 'ONETIME' OR rcs.status = 'ACTIVE')
@@ -95,7 +95,7 @@ if(!empty($add_to_charge)){
 }
 header('Content-Type: application/liquid');
 echo "<!-- ".print_r($res, true)." -->";
-echo "<!-- ".print_r($product, true)." -->";
+echo "<!-- ".print_r($variant, true)." -->";
 ?>
 
 {% assign portal_page = 'lander-addtobox' %}
