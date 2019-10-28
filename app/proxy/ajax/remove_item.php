c09de2561e4183549c3c6dd24acb153441492b32
Tim Nolan <j.tim.nolan@gmail.com>
2019-10-28 11:38:43 -0700

    add syncback on remove from box ajax

--- app/proxy/ajax/remove_item.php
+++ app/proxy/ajax/remove_item.php
@@ -4,12 +4,18 @@ global $db, $rc;
 $res = $rc->get('/subscriptions/'.intval($_REQUEST['id']));
 if(empty($res['subscription']) || $res['subscription']['status'] == 'ONETIME'){
 	$res = $rc->delete('/onetimes/'.intval($_REQUEST['id']));
+	$stmt = $db->prepare("UPDATE rc_subscriptions SET deleted_at=? WHERE recharge_id=?");
+	$stmt->execute([
+		date('Y-m-d H:i:s'),
+		intval($_REQUEST['id']),
+	]);
 } else {
 	$res = $this_res = $rc->post('/subscriptions/'.intval($_REQUEST['id']).'/cancel',[
 		'cancellation_reason' => 'Item removed from customer account',
 		'send_email' => 'false',
 		'commit_update' => true,
 	]);
+	insert_update_rc_subscription($db, $this_res['subscription'], $rc, $sc);
 }
 log_event($db, 'SUBSCRIPTION', $_REQUEST['id'], 'CANCEL', 'Item removed from customer account', 'Cancelled via user account: '.json_encode($res), 'Customer');
 
