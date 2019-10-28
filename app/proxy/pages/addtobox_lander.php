6547bc8a0ba61bbf5fa05ed0940bf1f45725f9a1
Tim Nolan <j.tim.nolan@gmail.com>
2019-10-28 11:57:29 -0700

    fix addtobox using wrong variant image

--- app/proxy/pages/addtobox_lander.php
+++ app/proxy/pages/addtobox_lander.php
@@ -112,7 +112,11 @@ echo "<!-- ".print_r($variant, true)." -->";
                 <div class="sc-lander-image">
                     {% for variant in all_products['<?= $product['handle'] ?>'].variants %}
                     {% if variant.id != <?=$variant['shopify_id']?> %}{% continue %}{% endif %}
-                        <img class="lazyload" data-srcset="{{ variant.image | img_url: 'x280' }} 1x, {{ variant.image | img_url: 'x280', scale: 2 }} 2x" />
+                        {% if variant.image != nil %}
+                            <img class="lazyload" data-srcset="{{ variant.image | img_url: 'x280' }} 1x, {{ variant.image | img_url: 'x280', scale: 2 }} 2x" />
+                        {% else %}
+                            <img class="lazyload" data-srcset="{{ all_products['<?= $product['handle'] ?>'].featured_image | img_url: 'x280' }} 1x, {{ all_products['<?= $product['handle'] ?>'].featured_image | img_url: 'x280', scale: 2 }} 2x" />
+                        {% endif %}
                     {% endfor %}
                 </div>
                 <div class="sc-lander-note">
@@ -125,7 +129,11 @@ echo "<!-- ".print_r($variant, true)." -->";
                 <div class="sc-lander-image">
                     {% for variant in all_products['<?= $product['handle'] ?>'].variants %}
                     {% if variant.id != <?=$variant['shopify_id']?> %}{% continue %}{% endif %}
-                        <img class="lazyload" data-srcset="{{ variant.image | img_url: 'x280' }} 1x, {{ variant.image | img_url: 'x280', scale: 2 }} 2x" />
+                        {% if variant.image != nil %}
+                            <img class="lazyload" data-srcset="{{ variant.image | img_url: 'x280' }} 1x, {{ variant.image | img_url: 'x280', scale: 2 }} 2x" />
+                        {% else %}
+                            <img class="lazyload" data-srcset="{{ all_products['<?= $product['handle'] ?>'].featured_image | img_url: 'x280' }} 1x, {{ all_products['<?= $product['handle'] ?>'].featured_image | img_url: 'x280', scale: 2 }} 2x" />
+                        {% endif %}
                     {% endfor %}
                 </div>
                 <div class="sc-lander-note">
