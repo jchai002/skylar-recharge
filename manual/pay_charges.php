17ddfc46ac86c9af91aca28f47bc2e4cf5ef601a
Tim Nolan <j.tim.nolan@gmail.com>
2019-11-01 06:39:34 -0700

    auto load charges to pay

--- manual/pay_charges.php
+++ manual/pay_charges.php
@@ -8,10 +8,43 @@ $scent = null;
 $cookie_token = '29422|7cbe63b01b7aec3588970e96fec94d101db71f8b0f72623bd7d3d2b87095ca3fcc767803605372a39b2ec76d62438ca4fa918f7b2ee735e6af4efb3505169b49';
 $cookie_session = 'eJw1j1FrgzAURv_KyHMf6l2zbsIeBmFD4V5piZPkRVqXqjFxI1qmlv73dYO9nKfvwHcurDwFMzQsHsPZrFjZfrD4wu6OLGYkmk4L5xSknpaXBSG1KOqZYAfav_us2HuUHSdJrbbUaK8WBfmMdsczmXZK1px8zlG4FqVzJPMIl3qNQm1Q7O3NcShfWyySiN4S0LKa1HLby-SbvG6yIue0pG0m1b2WasJCd2hdRwJ5JqpZ29TqIgfl8Zldb9-_TPCH3vTjf00wVXMItSl7M43l0Qy_OH0Gw-KIP2xhvYXHaMXOgwl_4QyeNgDs-gNWeVvh.EH_CiQ.Xj9IAxK8ioOVx6RCG-_UtwyezOM';
 
+$start_date = date('Y-m-d', strtotime('-1 day'));
+$end_date = date('Y-m-d', strtotime('+1 day'));
 $charge_ids = [];
+$charges = [];
+$start_time = microtime(true);
+do {
+	$page++;
+	// Load month's upcoming queued charges
+	$res = $rc->get('/charges', [
+		'date_min' => $start_date,
+		'date_max' => $end_date,
+		'status' => 'QUEUED',
+		'limit' => 250,
+		'page' => $page,
+//        'address_id' => '29806558',
+	]);
+	if(empty($res['charges'])){
+		print_r($res);
+		sleep(5);
+		$page--;
+	}
+	foreach($res['charges'] as $charge){
+		if(strtotime($charge['scheduled_at']) != strtotime(date('Y-m-d'))){
+			echo "Skipping charge ".$charge['id'].", scheduled for ".$charge['scheduled_at'].PHP_EOL;
+			continue;
+		}
+		$charges[] = $charge;
+	}
+	echo "Adding ".count($res['charges'])." to array - total: ".count($charges).PHP_EOL;
+	echo "Rate: ".(count($charges) / (microtime(true) - $start_time))." charges/s".PHP_EOL;
+} while(count($res['charges']) == 250);
+echo "Total: ".count($charges).PHP_EOL;
+
 
 $starttime = microtime(true);
-foreach($charge_ids as $rownum => $charge_id){
+foreach($charges as $rownum => $charge){
+	$charge_id = $charge['id'];
 	echo $charge_id.": ";
 
 	$ch = curl_init("https://maven-and-muse.shopifysubscriptions.com/charge/$charge_id/pay");
