a4b192dfb5c3fe6ab6955c5f4dce6f13c12e0340
Tim Nolan <j.tim.nolan@gmail.com>
2019-11-07 17:10:38 -0800

    Add previous charge count and update sub item index in scheduler

--- src/SubscriptionSchedule.php
+++ src/SubscriptionSchedule.php
@@ -213,11 +213,12 @@ class SubscriptionSchedule {
 				$item['scheduled_at'] = date('Y-m-d', $next_charge_time);
 				$item['scheduled_at_time'] = $next_charge_time;
 				$item['index'] = $subscription_index + $item['previous_charge_count'];
-				// TODO: If expires, add what number in series it is
+				if(!empty($item['expire_after_specific_number_of_charges']) && $item['index'] > $item['expire_after_specific_number_of_charges']){
+					break;
+				}
 				$this->add_item_to_schedule($item);
 
 				$subscription_index++;
-				// TODO: Stop if subscription is expired
 				$next_charge_time = self::get_subscription_time_by_index($subscription_index, $charge_time, $subscription['order_interval_frequency'], $subscription['order_interval_unit'], $subscription['order_interval_index']);
 				if($subscription_index > 100){
 					throw new Exception('Too many loops');
