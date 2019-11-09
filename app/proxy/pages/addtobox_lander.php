16fbe0b9068ebf8fc21d7b61aa5440eea7ee23a4
Tim Nolan <j.tim.nolan@gmail.com>
2019-11-08 17:01:05 -0800

    add additional loggint to addtobox lander

--- app/proxy/pages/addtobox_lander.php
+++ app/proxy/pages/addtobox_lander.php
@@ -92,7 +92,7 @@ if(!empty($add_to_charge)){
 				insert_update_rc_subscription($db, $res['onetime'], $rc, $sc);
 			}
         }
-		log_event($db, 'SUBSCRIPTION', $res, 'QUICK_ADDED', $_REQUEST, '', 'customer');
+		log_event($db, 'SUBSCRIPTION', $res, 'QUICK_ADDED', $_REQUEST, getallheaders(), 'customer');
     }
 }
 header('Content-Type: application/liquid');
