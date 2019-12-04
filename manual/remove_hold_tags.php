e0ddb18cea196f574333e59c06ac3d0d21ec2166
Tim Nolan <j.tim.nolan@gmail.com>
2019-12-04 12:50:56 -0800

    add presync to remove script

--- manual/remove_hold_tags.php
+++ manual/remove_hold_tags.php
@@ -2,8 +2,27 @@
 require_once(__DIR__.'/../includes/config.php');
 
 $start_date = date('Y-m-d');
+$start_time = time();
+$page = 0;
+$page_size = 250;
+$total_orders = 0;
+do {
+	$page++;
+	$orders = $sc->get('/admin/orders.json', [
+		'created_at_min' => $start_date,
+		'limit' => $page_size,
+		'page' => $page,
+		'order' => 'created_at asc',
+	]);
+	foreach($orders as $order){
+		$total_orders++;
+		echo insert_update_order($db, $order, $sc).PHP_EOL;
+	}
+	$elapsed_time = (time()-$start_time)/60;
+	echo "Synced $total_orders in ".round($elapsed_time, 2)."m ".round($total_orders/($elapsed_time), 2)." orders/min".PHP_EOL;
+} while(count($orders) >= $page_size);
 
-$stmt = $db->query("SELECT shopify_id as id, tags FROM orders WHERE tags LIKE '%HOLD: Test Order%'");
+$stmt = $db->query("SELECT shopify_id as id, tags FROM orders WHERE tags LIKE '%HOLD: Test Order%' AND created_at >= '".$start_date."'");
 
 $orders = $stmt->fetchAll();
 
