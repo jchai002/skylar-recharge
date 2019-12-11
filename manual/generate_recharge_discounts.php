e031c54ba43c082de18e83d7191fc65cfd677419
Tim Nolan <j.tim.nolan@gmail.com>
2019-12-10 16:52:38 -0800

    fix rc generate discount issue

--- manual/generate_recharge_discounts.php
+++ manual/generate_recharge_discounts.php
@@ -1,10 +1,10 @@
 <?php
 require_once(__DIR__.'/../includes/config.php');
 
-$start_from = 2;
-$num_to_generate = 500;
-$discount_template_id = 21526658;
-$prefix = "GS-50-";
+$start_from = 0;
+$num_to_generate = 25;
+$discount_template_id = 21645629;
+$prefix = "RT-20-";
 
 $res = $rc->get('/discounts/'.$discount_template_id);
 print_r($res);
@@ -12,7 +12,21 @@ if(empty($res['discount'])){
 	die('Discount template not found');
 }
 $discount_template = $res['discount'];
-unset($discount_template['id']);
+foreach([
+	'id', 'created_at', 'updated_at', 'times_used',
+] as $remove_key){
+	unset($discount_template[$remove_key]);
+}
+foreach([
+	'applies_to', 'applies_to_id', 'applies_to_product_type', 'applies_to_resource', 'ends_at', 'starts_at', 'once_per_customer', 'status', 'usage_limit', 'prerequisite_subtotal_min', 'duration_usage_limit',
+] as $remove_key){
+	if(array_key_exists($remove_key, $discount_template) && $discount_template[$remove_key] === null){
+		unset($discount_template[$remove_key]);
+	}
+}
+if($prefix == 'RT-20-'){
+	unset($discount_template['applies_to_product_type']);
+}
 
 mt_srand($discount_template_id);
 
@@ -23,16 +37,19 @@ for($i = 0; $i < $num_to_generate; $i++){
 	if($i < $start_from){
 		continue;
 	}
-	$codes[] = [
-		'code' => $code,
-	];
+	$codes[] = $code;
 }
 $outstream = fopen("rc_discounts.csv", 'w');
 
 foreach($codes as $index=>$code){
-	$discount_template['code'] = $code['code'];
+	$discount_template['code'] = $code;
 	$res = $rc->post('/discounts', $discount_template);
+	if(empty($res['discount'])){
+		print_r($res);
+		die("exiting at index $index code $code".PHP_EOL);
+	}
 	$discount = $res['discount'];
+	echo "Created ".$discount['code'].PHP_EOL;
 	if($index === 0){
 		$keys = array_keys($discount);
 		fputcsv($outstream, $keys);
