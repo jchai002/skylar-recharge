bc9d5bcfc5a85e89b577e8fccf58cbe28770d17c
Tim Nolan <j.tim.nolan@gmail.com>
2019-12-12 14:16:18 -0800

    add metaafields pull to product hook

--- hooks/product_updated.php
+++ hooks/product_updated.php
@@ -16,3 +16,20 @@ if(empty($product)){
 }
 
 echo insert_update_product($db, $product);
+
+$metafields = $sc->get('/admin/products/'.$product['id'].'/metafields.json');
+if(empty($metafields)){
+	print_r($sc->last_error);
+	echo "Couldn't get metafields for ".$product['id'].PHP_EOL;
+} else {
+	print_r(insert_update_metafields($db, $metafields));
+}
+foreach($product['variants'] as $variant){
+	$metafields = $sc->get('/admin/variants/'.$variant['id'].'/metafields.json');
+	if(empty($metafields)){
+		print_r($sc->last_error);
+		echo "Couldn't get metafields for ".$variant['id'].PHP_EOL;
+		continue;
+	}
+	print_r(insert_update_metafields($db, $metafields));
+}
