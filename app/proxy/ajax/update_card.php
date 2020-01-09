f82768c8bdf021e7f8f73501db3e00fc457008b1
Tim Nolan <j.tim.nolan@gmail.com>
2020-01-09 10:34:59 -0800

    add error catch for declined cards

--- app/proxy/ajax/update_card.php
+++ app/proxy/ajax/update_card.php
@@ -13,49 +13,16 @@ $res_all[] = $res = $rc->get('/customers/', [
 
 \Stripe\Stripe::setApiKey($_ENV['STRIPE_API_KEY']);
 
-if(empty($res['customers'])){
-	$res_all[] = $res = $stripe_customer = \Stripe\Customer::retrieve($customer['stripe_customer_token']);
-	$stripe_customer->source = $token;
-	$stripe_customer->save();
+try {
+	if(empty($res['customers'])){
+		$res_all[] = $res = $stripe_customer = \Stripe\Customer::retrieve($customer['stripe_customer_token']);
+		$stripe_customer->source = $token;
+		$stripe_customer->save();
 
-	$shopify_customer = $sc->get('/admin/customers/'.$_REQUEST['c'].'.json');
-	$res_all[] = $res = $rc->post('/customers/',[
-		'shopify_customer_id' => $_REQUEST['c'],
-		'email' => $shopify_customer['email'],
-		'first_name' => $_REQUEST['billing_first_name'],
-		'last_name' => $_REQUEST['billing_last_name'],
-		'billing_first_name' => $_REQUEST['billing_first_name'],
-		'billing_last_name' => $_REQUEST['billing_last_name'],
-		'billing_address1' => $_REQUEST['billing_address1'],
-		'billing_address2' => $_REQUEST['billing_address2'],
-		'billing_zip' => $_REQUEST['billing_zip'],
-		'billing_phone' => $_REQUEST['billing_zip'],
-		'billing_city' => $_REQUEST['billing_city'],
-		'billing_province' => $_REQUEST['billing_province'],
-		'billing_country' => $_REQUEST['billing_country'],
-		'stripe_customer_token' => $stripe_customer->id,
-	]);
-	if(!empty($res['error'])){
-		echo json_encode([
-			'success' => false,
-			'error' => $res['error'],
-			'res' => $res_all,
-		]);
-	} else {
-		echo json_encode([
-			'success' => true,
-			'res' => $res_all
-		]);
-	}
-} else {
-	$customer = $res['customers'][0];
-	if($customer['processor_type'] != 'stripe'){
-		$res_all[] = $res = $stripe_customer = \Stripe\Customer::create([
-			'email' => $customer['email'],
-			'source' => $token,
-		]);
-		$res_all[] = $res = $rc->put('/customers/'.$customer['id'],[
-			'stripe_customer_token' => $stripe_customer->id,
+		$shopify_customer = $sc->get('/admin/customers/' . $_REQUEST['c'] . '.json');
+		$res_all[] = $res = $rc->post('/customers/', [
+			'shopify_customer_id' => $_REQUEST['c'],
+			'email' => $shopify_customer['email'],
 			'first_name' => $_REQUEST['billing_first_name'],
 			'last_name' => $_REQUEST['billing_last_name'],
 			'billing_first_name' => $_REQUEST['billing_first_name'],
@@ -67,17 +34,60 @@ if(empty($res['customers'])){
 			'billing_city' => $_REQUEST['billing_city'],
 			'billing_province' => $_REQUEST['billing_province'],
 			'billing_country' => $_REQUEST['billing_country'],
+			'stripe_customer_token' => $stripe_customer->id,
 		]);
+		if(!empty($res['error'])){
+			echo json_encode([
+				'success' => false,
+				'error' => $res['error'],
+				'res' => $res_all,
+			]);
+		} else {
+			echo json_encode([
+				'success' => true,
+				'res' => $res_all
+			]);
+		}
 	} else {
-		$res_all[] = $res = $stripe_customer = \Stripe\Customer::retrieve($customer['stripe_customer_token']);
-		$stripe_customer->source = $token;
-		$stripe_customer->invoice_settings->default_payment_method = null;
-		$stripe_customer->save();
+		$customer = $res['customers'][0];
+		if($customer['processor_type'] != 'stripe'){
+			$res_all[] = $res = $stripe_customer = \Stripe\Customer::create([
+				'email' => $customer['email'],
+				'source' => $token,
+			]);
+			$res_all[] = $res = $rc->put('/customers/' . $customer['id'], [
+				'stripe_customer_token' => $stripe_customer->id,
+				'first_name' => $_REQUEST['billing_first_name'],
+				'last_name' => $_REQUEST['billing_last_name'],
+				'billing_first_name' => $_REQUEST['billing_first_name'],
+				'billing_last_name' => $_REQUEST['billing_last_name'],
+				'billing_address1' => $_REQUEST['billing_address1'],
+				'billing_address2' => $_REQUEST['billing_address2'],
+				'billing_zip' => $_REQUEST['billing_zip'],
+				'billing_phone' => $_REQUEST['billing_zip'],
+				'billing_city' => $_REQUEST['billing_city'],
+				'billing_province' => $_REQUEST['billing_province'],
+				'billing_country' => $_REQUEST['billing_country'],
+			]);
+		} else {
+			$res_all[] = $res = $stripe_customer = \Stripe\Customer::retrieve($customer['stripe_customer_token']);
+			$stripe_customer->source = $token;
+			$stripe_customer->invoice_settings->default_payment_method = null;
+			$stripe_customer->save();
+		}
 	}
+} catch (\Stripe\Error\Card $e){
+	$error = $e;
 }
-
-
-echo json_encode([
-	'success' => true,
-	'res' => $res_all,
-]);
+if(!empty($error)){
+	echo json_encode([
+		'success' => false,
+		'res' => $res_all,
+		'error' => $error,
+	]);
+} else {
+	echo json_encode([
+		'success' => true,
+		'res' => $res_all,
+	]);
+}
