f3b99a9374b6ebbb61394ed89d403d823c4a47ce
Tim Nolan <j.tim.nolan@gmail.com>
2020-01-08 16:12:56 -0800

    update default card loading from stripe

--- app/proxy/pages/billing.php
+++ app/proxy/pages/billing.php
@@ -21,11 +21,17 @@ if(!empty($main_sub)){
 		if($customer['processor_type'] == 'stripe'){
 			\Stripe\Stripe::setApiKey($_ENV['STRIPE_API_KEY']);
 			$stripe_customer = \Stripe\Customer::retrieve($customer['stripe_customer_token']);
-			if(!empty($stripe_customer->default_source)){
-				foreach($stripe_customer->sources->data as $source){
-					if($source->id == $stripe_customer->default_source){
-						$cc_info = $source;
-						break;
+			try {
+                if(!empty($stripe_customer->invoice_settings->default_payment_method)){
+                    $cc_info = \Stripe\PaymentMethod::retrieve($stripe_customer->invoice_settings->default_payment_method)->card;
+                }
+			} catch(\Stripe\Error\InvalidRequest $e){
+				if(!empty($stripe_customer->default_source)){
+					foreach($stripe_customer->sources->data as $source){
+						if($source->id == $stripe_customer->default_source){
+							$cc_info = $source;
+							break;
+						}
 					}
 				}
 			}
