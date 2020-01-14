8092f413002512fc1f456f1c2536019cf1bfc09c
Tim Nolan <j.tim.nolan@gmail.com>
2020-01-14 12:08:16 -0800

    add sub promos to sku update

--- manual/sc_update_promo_sku.php
+++ manual/sc_update_promo_sku.php
@@ -1,11 +1,52 @@
 <?php
 require_once(__DIR__.'/../includes/config.php');
 
-$new_sku = $db->query("SELECT sku FROM sc_product_info WHERE sc_date = '".date('Y-m', get_next_month())."-01'")->fetchColumn();
+$monthly_scent = sc_get_monthly_scent($db, get_next_month(), true);
+if(empty($monthly_scent)){
+	die("Monthly scent not set!");
+}
+$new_sku = $monthly_scent['sku'];
 if(empty($new_sku)){
 	die("No sku!");
 }
+
 echo "Changing all promotions ".date('Y-m-d')." to ".date('Y-m-t')." to $new_sku".PHP_EOL;
+
+echo "Updating subscriptions".PHP_EOL;
+
+$page = 0;
+$subscriptions = [];
+do {
+	$page++;
+	$res = $rc->get('/subscriptions', [
+		'page' => $page,
+		'limit' => 250,
+		'shopify_variant_id' => 28003712663639,
+		'scheduled_at_min' => date('Y-m-d'),
+		'scheduled_at_max' => date('Y-m-t'),
+	]);
+	foreach($res['subscriptions'] as $subscription){
+		if(!is_scent_club_promo(get_product($db, $subscription['line_items'][0]['shopify_product_id']))){
+			echo "Skipping subscription because not a promo ".$subscription['id']." ".$subscription['email'].PHP_EOL;
+		}
+		$subscriptions[] = $subscription;
+	}
+} while(count($res['orders']) >= 250);
+
+echo "Updating ".count($subscriptions)." orders".PHP_EOL;
+foreach($subscriptions as $subscription){
+	$res = $rc->put('/subscriptions/'.$subscription['id'], [
+		'sku' => $new_sku,
+	]);
+	if(empty($res['subscription']) ||$res['subscription']['sku'] != $new_sku){
+		print_r($res);
+		echo "ERROR CANNOT UPDATE SUB";
+		die();
+	}
+}
+
+
+echo "Updating prepaid orders".PHP_EOL;
 
 $page = 0;
 $orders = [];
@@ -23,9 +64,8 @@ do {
 			echo "Not 1 line item!";
 			die();
 		}
-		if($order['line_items'][0]['shopify_variant_id'] != 28003712663639){
+		if(!is_scent_club_promo(get_product($db, $order['line_items'][0]['shopify_product_id']))){
 			echo "Skipping order because not a promo ".$order['id']." ".$order['email'].PHP_EOL;
-			die();
 		}
 		$orders[] = $order;
 	}
