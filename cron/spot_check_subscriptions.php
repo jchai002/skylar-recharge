13136b78abadbf7b95bab84defb9748592c261d3
Tim Nolan <j.tim.nolan@gmail.com>
2020-01-14 16:04:03 -0800

    add gift scripts to /scent_club

--- cron/spot_check_subscriptions.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once dirname(__FILE__).'/../includes/config.php';
-require_once dirname(__FILE__).'/../includes/class.RechargeClient.php';
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-
-$rc = new RechargeClient();
