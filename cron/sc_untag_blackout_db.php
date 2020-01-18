4e810d738f8d84a5a7dad492ee0c047302d13b35
Tim Nolan <j.tim.nolan@gmail.com>
2020-01-17 17:45:34 -0800

    move working pool to untag script

--- cron/sc_untag_blackout_db.php
+++ cron/sc_untag_blackout_db.php
@@ -1,5 +1,7 @@
 <?php
 
+use GuzzleHttp\Handler\CurlMultiHandler;
+use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Promise\PromiseInterface;
 use Psr\Http\Message\ResponseInterface;
 
@@ -39,20 +41,41 @@ $orders = $stmt->fetchAll();
 
 echo "Starting untagging on ".count($orders)." orders".PHP_EOL;
 
-// Make our own custom pool
+$curl = new CurlMultiHandler();
+$handler = HandlerStack::create($curl);
+$sc = new ShopifyClient([
+	'handler' => $handler,
+]);
+$sc->rate_buffer = 10;
+if(!empty($_ENV['SHOPIFY_UNTAG_KEYS'])){
+	$extra_keys = explode(',',$_ENV['SHOPIFY_UNTAG_KEYS']);
+	foreach($extra_keys as $extra_key){
+		$sc->addSecret(...explode(':', $extra_key));
+		echo "Adding Key ".$extra_key.PHP_EOL;
+	}
+}
+
 $promises = [];
-$buffer = 10;
+$max_concurrent = 20;
+$start_time = time();
+$orders_processed = 0;
+$last_output_at = 0;
+$last_reason_at = 0;
 while(!empty($orders) || !empty($promises)){
+//	echo "Starting pool loop".PHP_EOL;
+	$curl->tick();
 	// Handle and empty
 	/** @var PromiseInterface $promise */
 	foreach($promises as $i=>$promise){
 		if($promise->getState() != PromiseInterface::PENDING){
-			$promises[$i] = null;
+//			echo "Freeing Promise $i ".$promise->getState().PHP_EOL;
+			$promises[$i] = false;
+			$orders_processed++;
 		}
 	}
-	$promises = array_filter($promises);
+	$promises = array_values(array_filter($promises));
 	// Refill
-	while(!empty($orders) && count($promises) <= $sc->totalCallsLeft() - $buffer){
+	while(!empty($orders) && count($promises) < $max_concurrent && $sc->totalCallsLeft() > 0){
 		$order = array_pop($orders);
 
 		$tags = explode(', ', $order['tags']);
@@ -60,29 +83,41 @@ while(!empty($orders) || !empty($promises)){
 		if (false !== $key) {
 			unset($tags[$key]);
 		}
-		// TODO: Add retrying for failed calls
+		echo "Sending request for ".$order['id'].PHP_EOL;
 		$promises[] = $sc->putAsync('orders/'.$order['id'].'.json', ['order' => [
 			'id' => $order['id'],
 			'tags' => implode(', ', $tags),
 		]])->then(function(ShopifyResponse $response) use($db, $sc) {
 			$order = $response->getJson();
 			if(!empty($order)){
-				insert_update_order($db, $order, $sc);
+				echo "Processing response for ".$order['id'].PHP_EOL;
+//				insert_update_order($db, $order, $sc);
 			}
+		}, function(Exception $e){
+			print_r($e->getMessage());
+			die();
 		});
 	}
-	sleep(100);
+	if($last_reason_at != $orders_processed){
+		if(empty($sc->totalCallsLeft())){
+			$last_reason_at = $orders_processed;
+			echo "No calls left".PHP_EOL;
+		}
+		if(count($promises) >= $max_concurrent){
+			$last_reason_at = $orders_processed;
+			echo "Hit max concurrent".PHP_EOL;
+		}
+	}
+	usleep(50*1000);
+	if($orders_processed != $last_output_at && $orders_processed % 20 == 0){
+		$last_output_at = $orders_processed;
+		$time_elapsed = time() - $start_time;
+		echo "Tagged $orders_processed in $time_elapsed s, ".round($orders_processed/$time_elapsed, 2)." orders/s".PHP_EOL;
+	}
 }
 
+$time_elapsed = time() - $start_time;
+echo "Finished tagging $orders_processed orders in $time_elapsed s, ".round($orders_processed/$time_elapsed, 2)." orders/s".PHP_EOL;
 
-
-/*
-foreach($orders as $order){
-	$res = $sc->put('/admin/orders/'.$order['id'].'.json', ['order' => [
-		'id' => $order['id'],
-		'tags' => implode(', ', $tags),
-	]]);
-	echo " ".$res['tags'].PHP_EOL;
-	// TODO: insert_update order
-}
+// TODO: insert_update order
 // TODO: Create loop that re-checks
