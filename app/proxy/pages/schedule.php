bc9436ee7fe7f5589d9ce12460722a82531c049b
Tim Nolan <j.tim.nolan@gmail.com>
2020-01-30 10:06:21 -0800

    remove stars from sc gifts

--- app/proxy/pages/schedule.php
+++ app/proxy/pages/schedule.php
@@ -191,8 +191,8 @@ print_r($schedule->get());
                                                         <div class="sc-item-title"><?= empty($item['product_title']) ? $item['title'] : $item['product_title']?></div>
                                                         {% if box_variant.title != 'Default Title' %}<div class="sc-item-subtitle">{{ box_variant.title }}</div>{% endif %}
 													<?php } else if(is_scent_club_gift(get_product($db, $item['shopify_product_id']))){ ?>
-                                                        <div class="sc-item-title">Scent Club Gift*</div>
-                                                        <div class="sc-item-subtitle"><?= $item['index'] + 1 ?> of <?= $item['expire_after_specific_number_of_charges'] ?? "{{ box_product.variants.first.title }}" ?>*</div>
+                                                        <div class="sc-item-title">Scent Club Gift</div>
+                                                        <div class="sc-item-subtitle"><?= $item['index'] + 1 ?> of <?= $item['expire_after_specific_number_of_charges'] ?? "{{ box_product.variants.first.title }}" ?></div>
                                                         <div><a class="sc-swap-link" href="#"><img src="{{ 'icon-swap.svg' | file_url }}" alt="Swap scent icon" /> <span>Swap Scent</span></a></div>
                                                     <?php } else if(is_scent_club(get_product($db, $item['shopify_product_id']))){ ?>
                                                         <div class="sc-item-title">Skylar Scent Club</div>
