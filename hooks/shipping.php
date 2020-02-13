cbcb0d0904d51340251a3f1a0daa22ba7070cf3c
Tim Nolan <j.tim.nolan@gmail.com>
2020-02-13 15:51:43 -0800

    re-add sc shipping test

--- hooks/shipping.php
+++ hooks/shipping.php
@@ -202,7 +202,7 @@ switch($rate['destination']['country']){
 		*/
 		break;
 }
-/*
+
 if($is_test){
 	$_RATES[] = [
 		'service_name' => 'Scent Club Shipping (3-7 business days)',
@@ -212,7 +212,7 @@ if($is_test){
 		'currency' => 'USD',
 	];
 }
-*/
+
 if($free_override){
 	foreach($_RATES as $index=>$v){
 		$_RATES[$index]['total_price'] = 0;
