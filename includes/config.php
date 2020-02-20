2a1f07af4dd345effb65042fe169ba7bbe6a7291
Tim Nolan <j.tim.nolan@gmail.com>
2020-02-20 09:07:46 -0800

    change sub pricing to round down

--- includes/config.php
+++ includes/config.php
@@ -803,7 +803,7 @@ function get_subscription_price($product, $variant){
 		return 40;
 	}
 	if(strpos($product['type'], 'Body') !== false){
-		return round($variant['price']*.9);
+		return floor($variant['price']*.9);
 	}
 	return round($variant['price']*.9, 2);
 }
