1a601e2f3fa0f86dcb07a3cbf34a13ca4a7af062
Tim Nolan <j.tim.nolan@gmail.com>
2020-02-22 13:27:27 -0800

    longer window for error charges

--- manual/regenerate_error_charges.php
+++ manual/regenerate_error_charges.php
@@ -21,7 +21,7 @@ do {
 	$res = $rc->get('/charges', [
 		'status' => 'ERROR',
 		'date_min' => date('Y-m-d', strtotime('yesterday')),
-		'date_max' => date('Y-m-d', get_month_by_offset(2)),
+		'date_max' => date('Y-m-d', get_month_by_offset(3)),
 //		'date_min' => date('Y-m-d', get_next_month()),
 		'page' => $page,
 		'limit' => 250,
@@ -51,7 +51,7 @@ do {
 //die();
 
 $cookie_token = '29422|7cbe63b01b7aec3588970e96fec94d101db71f8b0f72623bd7d3d2b87095ca3fcc767803605372a39b2ec76d62438ca4fa918f7b2ee735e6af4efb3505169b49';
-$cookie_session = '.eJw1j8FqwzAQRH-l7LkHW_Glhl6KTFFhZSKcmt1LaBvXsmSF4CTYVsi_Vy2UOQxzGObNDfbfU3e2UF6ma_cI--EA5Q0ePqEElpijHEeWL46isfp1u6Crch12Gx3MWEsz1LJfKWBOjlZuVabbamFpPEqfcWMHdrhQW80ov2Zs_AYbKnT7lroq14I9xd2c8kDBDBjZajl6cgeXNi0JtWLsCxQqcSSPv-ozat69Fqrghn3dassBn-Ge2E_dFD6O3fHy_-Z67qa_RyCeCiHg_gMUDU8G.ERXzlw.h7lppIZOi5JWxTujHGMNSPgo3Mw';
+
 $last_charge_id = 0;
 $starttime = microtime(true);
 foreach($charges as $rownum => $charge){
@@ -60,17 +60,7 @@ foreach($charges as $rownum => $charge){
 
 	echo $charge_id.", ".$charge['address_id'].": ";
 
-//	die();
-
-	$ch = curl_init("https://maven-and-muse.shopifysubscriptions.com/charge/$charge_id/regenerate_charge/");
-	curl_setopt_array($ch, [
-		CURLOPT_RETURNTRANSFER =>  true,
-		CURLOPT_HTTPHEADER => ["cookie: session=$cookie_session"],
-//		CURLOPT_HTTPHEADER => ["cookie: remember_token=$cookie_token"],
-	]);
-	$res = curl_exec($ch);
-
-	print_r($res);
+	print_r(regenerate_charge($charge_id));
 
 	if($rownum % 20 == 0){
 		echo "Row: $rownum. Time: ".(microtime(true)-$starttime)." seconds | ";
