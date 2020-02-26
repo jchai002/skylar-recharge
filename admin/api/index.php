563383c44283624556f76d316d0c0629dbfde0b4
Tim Nolan <j.tim.nolan@gmail.com>
2020-02-26 11:50:56 -0800

    add admin api proxy for adrian

--- admin/api/index.php
+++ admin/api/index.php
@@ -5,4 +5,12 @@ $sc = new ShopifyClient([], $_ENV['SHOPIFY_UTILS_APP_TOKEN']);
 
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
-echo json_encode($sc->call($_SERVER['REQUEST_METHOD'], $_REQUEST['path'], $_REQUEST['data']));
+try {
+	echo json_encode($sc->call($_SERVER['REQUEST_METHOD'], $_REQUEST['path'] ?? '', $_REQUEST['data'] ?? []));
+} catch (Exception $e) {
+	echo json_encode([
+		'success' => false,
+		'error' => $e->getMessage(),
+		'code' => $e->getCode(),
+	]);
+}
