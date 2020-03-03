c654bacd31d459b48f376e2de7440376b732f5ff
Tim Nolan <j.tim.nolan@gmail.com>
2020-03-03 14:26:20 -0800

    add minute limit on update sends

--- cron/cin_unhold_orders.php
+++ cron/cin_unhold_orders.php
@@ -1242,6 +1242,23 @@ do {
 
 	$stmt = $db->prepare("SELECT * FROM orders WHERE number=?");
 	foreach($cc_orders as $index=>$cc_order){
+		$send_updates = false;
+		if(count($updates) == $page_size){
+			echo "Updates hit $page_size, ";
+			$send_updates = true;
+		}
+		if(count($updates) > 0 && time()-$last_send_time > 30){
+			echo "It's been ".(time()-$last_send_time)."s since last update, ";
+			$send_updates = true;
+		}
+		if($send_updates){
+			echo "Sending updates... ";
+			$res = send_cc_updates($cc, $updates);
+			$updates = [];
+			echo "Done".PHP_EOL;
+			sleep(1);
+			$last_send_time = time();
+		}
 		echo "Checking order ".$cc_order['reference']."... ";
 		if($cc_order['logisticsStatus'] != 9){
 			if(!empty($row['cancelled_at'])){
@@ -1272,23 +1289,6 @@ do {
 		$cc_order['branchId'] = calc_branch_id($cc_order);
 		$updates[] = $cc_order;
 		echo "Added to update queue w/ branch id ".$cc_order['branchId']." [".count($updates)."]".PHP_EOL;
-		$send_updates = false;
-		if(count($updates) == $page_size){
-			echo "Updates hit $page_size, ";
-			$send_updates = true;
-		}
-		if(count($updates) > 0 && time()-$last_send_time > 60){
-			echo "It's been ".$last_send_time-time()."s since last update, ";
-			$send_updates = true;
-		}
-		if($send_updates){
-			echo "Sending updates... ";
-			$res = send_cc_updates($cc, $updates);
-			$updates = [];
-			echo "Done".PHP_EOL;
-			sleep(1);
-			$last_send_time = time();
-		}
 	}
 } while(count($cc_orders) >= $page_size);
 
