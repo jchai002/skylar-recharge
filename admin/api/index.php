5722b02780b3aff77b5ac90ce34516363c6d32ab
Tim Nolan <j.tim.nolan@gmail.com>
2020-03-12 16:25:21 -0700

    add post support

--- admin/api/index.php
+++ admin/api/index.php
@@ -1,21 +1,6 @@
 <?php
 require_once(__DIR__.'/../../includes/config.php');
 
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-	// return only the headers and not the content
-	// only allow CORS if we're doing a GET - i.e. no saving for now.
-	if (
-		isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])
-		&& $_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'] == 'GET'
-		&& isset($_SERVER['HTTP_ORIGIN'])
-//		&& is_approved($_SERVER['HTTP_ORIGIN'])
-	) {
-		header('Access-Control-Allow-Origin: *');
-		header('Access-Control-Allow-Headers: X-Requested-With');
-	}
-	exit;
-}
-
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
 	$data = file_get_contents('php://input');
 	$data = json_decode($data, true);
