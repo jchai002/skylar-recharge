2877e721f6a0419f6b9ce35762ca01cd5b25967e
Tim Nolan <j.tim.nolan@gmail.com>
2020-03-12 16:48:49 -0700

    add post support

--- admin/api/index.php
+++ admin/api/index.php
@@ -24,9 +24,11 @@ $sc = new ShopifyClient([], $_ENV['SHOPIFY_UTILS_APP_TOKEN']);
 $path = $_REQUEST['path'] ?? str_replace('/admin/api/', '', parse_url($_SERVER['REQUEST_URI'])['path']);
 
 header('Content-Type: application/json');
-$result = json_decode($_REQUEST['payload'], true);
-if(json_last_error() === JSON_ERROR_NONE){
-	$_REQUEST['payload'] = $result;
+if(is_string($_REQUEST['payload'])){
+	$result = json_decode($_REQUEST['payload'], true);
+	if(json_last_error() === JSON_ERROR_NONE){
+		$_REQUEST['payload'] = $result;
+	}
 }
 try {
 	echo json_encode(['response' => $sc->call($_REQUEST['method'] ?? $_SERVER['REQUEST_METHOD'], $path, $_REQUEST['payload'] ?? []), 'payload' => $_REQUEST['payload'], 'path' => $path]);
