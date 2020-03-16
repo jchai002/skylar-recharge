153d7abc5e72cfb07e50bdd426f2dd649bcd56d3
Tim Nolan <j.tim.nolan@gmail.com>
2020-03-16 12:12:11 -0700

    test admin tracker pop

--- app/alias/tracking.php
+++ app/alias/tracking.php
@@ -1,4 +1,4 @@
 <script>
 	window.open("https://skylar.com/tools/skylar/tracking/<?=$_REQUEST['id']?>");
-	window.location = "https://maven-and-muse.myshopify.com/admin/orders/<?=$_REQUEST['id']?>";
+	window.history.back();
 </script>
