1cfef567a5ecd4bfbf1a6d54210401f71581534f
Tim Nolan <j.tim.nolan@gmail.com>
2020-03-19 16:12:38 -0700

    fix swaps not showing correct freq

--- app/proxy/pages/schedule.php
+++ app/proxy/pages/schedule.php
@@ -218,7 +218,7 @@ print_r($schedule->get());
                                                 <div>
                                                     <div class="sc-item-detail-label">Delivery</div>
                                                     <div class="sc-item-detail-value">
-                                                        <?php if(empty($item['order_interval_frequency'])){ ?>
+                                                        <?php if(empty($item['order_interval_frequency']) || $item['status'] == 'ONETIME'){ ?>
                                                             Once
                                                         <?php } else if($item['order_interval_frequency'] == '1'){ ?>
                                                             Every <?=$item['order_interval_unit']?>
