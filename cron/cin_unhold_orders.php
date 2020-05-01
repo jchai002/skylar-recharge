49a6f8c5fbf86e177f3c6528db9d09a70398f89a
Tim Nolan <j.tim.nolan@gmail.com>
2020-05-01 06:56:14 -0700

    switch from status to stage in cin7 unhold pull

--- cron/cin_unhold_orders.php
+++ cron/cin_unhold_orders.php
@@ -1238,7 +1238,7 @@ do {
 	$res = $cc->get('SalesOrders', [
 		'query' => [
 			'fields' => implode(',', ['id', 'email', 'status', 'reference', 'logisticsStatus', 'freightDescription', 'deliveryPostalCode', 'deliveryCountry', 'lineItems']),
-			'where' => "LogisticsStatus = '9' AND createdDate >= '$cut_on_date' AND createdDate < '$buffer_date' AND stage = 'APPROVED' AND stage = 'New'",
+			'where' => "LogisticsStatus = '9' AND createdDate >= '$cut_on_date' AND createdDate < '$buffer_date' AND status = 'APPROVED' AND stage = 'New'",
 			'order' => 'CreatedDate DESC',
 			'rows' => $page_size,
 			'page' => $page,
