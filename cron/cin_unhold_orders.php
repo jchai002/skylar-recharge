87087fce45c7cd89405998e2cd7241e530bde285
Tim Nolan <j.tim.nolan@gmail.com>
2020-05-01 05:42:21 -0700

    fix hardcoded alert id

--- cron/cin_unhold_orders.php
+++ cron/cin_unhold_orders.php
@@ -1299,12 +1299,13 @@ do {
 		switch($cc_order['branchId']){
 			default: break;
 			case -1:
+				// TODO: figure out why this doesn't get suppressed
 				echo "Order doesn't have zip code, skipping and alerting".PHP_EOL;
-				send_alert($db, 13, "Order is being held because it doesn't have a shipping address zip: https://go.cin7.com/Cloud/TransactionEntry/TransactionEntry.aspx?idCustomerAppsLink=800541&OrderId=206888", 'Skylar Alert - No Zip on Order', ['tim@skylar.com', 'kristin@skylar.com']);
+				send_alert($db, 13, "Order is being held because it doesn't have a shipping address zip: https://go.cin7.com/Cloud/TransactionEntry/TransactionEntry.aspx?idCustomerAppsLink=800541&OrderId=".$cc_order['id'], 'Skylar Alert - No Zip on Order', ['tim@skylar.com', 'kristin@skylar.com']);
 				continue 2; // Switch statements are treated as loops
 			case -2:
 				echo "No branch can fulfill this order, skipping and alerting".PHP_EOL;
-				send_alert($db, 14, "Order is being held because it doesn't have stock available: https://go.cin7.com/Cloud/TransactionEntry/TransactionEntry.aspx?idCustomerAppsLink=800541&OrderId=206888", 'Skylar Alert - No Stock Available', ['tim@skylar.com', 'kristin@skylar.com']);
+				send_alert($db, 14, "Order is being held because it doesn't have stock available: https://go.cin7.com/Cloud/TransactionEntry/TransactionEntry.aspx?idCustomerAppsLink=800541&OrderId=".$cc_order['id'], 'Skylar Alert - No Stock Available', ['tim@skylar.com', 'kristin@skylar.com']);
 				continue 2; // Switch statements are treated as loops
 		}
 		$stmt_get_prev_order->execute([
