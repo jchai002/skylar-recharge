28e9e761642ea72e8bcd3da78640063d3ab300f1
Tim Nolan <j.tim.nolan@gmail.com>
2020-05-07 17:33:34 -0700

    add errors to order created

--- hooks/order_created.php
+++ hooks/order_created.php
@@ -1,10 +1,6 @@
 <?php
 require_once('../includes/config.php');
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 require_once(__DIR__.'/../includes/class.ShopifyClient.php');
 $scp = new ShopifyPrivateClient();
 
