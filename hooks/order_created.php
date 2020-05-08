e977d900ea11aa6b1b0f578192267813a539e9cd
Tim Nolan <j.tim.nolan@gmail.com>
2020-05-07 17:32:04 -0700

    add errors to order created

--- hooks/order_created.php
+++ hooks/order_created.php
@@ -5,7 +5,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$scp = new ShopifyPrivateClient();
+//$scp = new ShopifyPrivateClient();
 
 if(!empty($_REQUEST['id'])){
 	$order = $sc->call('GET', '/admin/orders/'.intval($_REQUEST['id']).'.json');
