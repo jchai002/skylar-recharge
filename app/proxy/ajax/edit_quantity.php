0701ffe2efd3ef1345fba9fa22503a4e64a364d7
Tim Nolan <j.tim.nolan@gmail.com>
2020-05-28 14:44:28 -0700

    check inventory quantity on edit

--- app/proxy/ajax/edit_quantity.php
+++ app/proxy/ajax/edit_quantity.php
@@ -5,21 +5,70 @@ global $sc, $db, $rc;
 
 $quantity = intval($_REQUEST['quantity']);
 
-$res = $rc->put('subscriptions/'.intval($subscription_id), [
-	'quantity' => $quantity,
-]);
+// Check if the new quantity is available
+$subscription = get_rc_subscription($db, $subscription_id, $rc, $sc);
+$subscription_uri = $subscription['status'] == 'ONETIME' ? 'onetime' : 'subscription';
 
+$qty_change = $quantity - $subscription['quantity'];
+if($qty_change > 0){
+	if(is_inventory_available($db, $subscription['shopify_variant_id'], $qty_change)){
 
-if(!empty($res['error'])){
-	echo json_encode([
-		'success' => false,
-		'error' => $res['error'],
-		'res' => $res,
-	]);
-} else {
-	echo json_encode([
-		'success' => true,
-		'res' => $res,
-		'id' => $res_id,
-	]);
+		$res = $rc->put($subscription_uri.'s/'.intval($subscription_id), [
+			'quantity' => $quantity,
+		]);
+
+		if(!empty($res[$subscription_uri])){
+			insert_update_rc_subscription($db, $res[$subscription_uri], $rc, $sc);
+		}
+		if(!empty($res['error'])){
+			echo json_encode([
+				'success' => false,
+				'error' => $res['error'],
+				'res' => $res,
+			]);
+		} else {
+			echo json_encode([
+				'success' => true,
+				'res' => $res,
+				'id' => $res_id,
+			]);
+		}
+	} else {
+		echo json_encode([
+			'success' => false,
+			'error' => "Not enough inventory available",
+			'res' => [],
+		]);
+	}
+}
+
+function is_inventory_available(PDO $db, $shopify_variant_id, $quantity = 1){
+	$variant = get_variant($db, $shopify_variant_id);
+	$product = get_product($db, $variant['shopify_product_id']);
+	if($product['type'] != 'Scent Club Month'){
+		return true;
+	}
+	$stmt = $db->prepare("SELECT v.id, v.shopify_id, v.sku, SUM(csu.available+csu.virtual) AS inventory_quantity, v.title, IFNULL(held_quantity,0) AS held_quantity, SUM(csu.available+csu.virtual)-(IFNULL(held_quantity,0))-20 AS stock_available_unreserved
+FROM sc_products scp
+LEFT JOIN variants v ON scp.variant_id=v.id
+LEFT JOIN products p ON v.product_id = p.id
+LEFT JOIN cin_product_options cpo ON v.sku=cpo.sku
+LEFT JOIN (
+	SELECT scp.variant_id, IFNULL(SUM(quantity), 0) AS held_quantity
+	FROM sc_products scp
+	LEFT JOIN rc_subscriptions rcs ON rcs.variant_id=scp.variant_id
+	WHERE rcs.status IN ('ACTIVE', 'ONETIME')
+	AND rcs.deleted_at IS NULL
+	AND rcs.next_charge_scheduled_at >= '".date('Y-m-d H:i:s')."'
+	GROUP BY scp.variant_id
+) rq ON rq.variant_id=scp.variant_id
+LEFT JOIN cin_stock_units csu ON cpo.id=csu.cin_product_option_id AND csu.cin_branch_id IN(3, 23755)
+WHERE v.shopify_id = ?
+;");
+	$stmt->execute([$shopify_variant_id]);
+	$row = $stmt->fetch();
+	if($row['stock_available_unreserved'] < $quantity){
+		return false;
+	}
+	return true;
 }
