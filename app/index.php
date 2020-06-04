4d316235633d0e65f4cdecb19ad238685c21c954
Tim Nolan <j.tim.nolan@gmail.com>
2020-06-03 17:39:19 -0700

    add install for dev app

--- app/index.php
+++ app/index.php
@@ -1,11 +1,13 @@
 <?php
 require_once('../includes/config.php');
+require_once('../includes/class.ShopifyClient.php');
 
 //echo "Skylar Shopify App - Created by Tim";
 
 //die();
+$sc = new ShopifyAppClient('maven-and-muse.myshopify.com', '', $_ENV['SKYLAR_UTILS_DEV_KEY'], $_ENV['SKYLAR_UTILS_DEV_SECRET']);
 if(isset($_GET['code'])) {
 	echo $sc->getAccessToken($_GET['code']);
 	exit;
 }
-echo $sc->getAuthorizeUrl(SHOPIFY_SCOPE, 'https://ec2production.skylar.com/app');
+echo $sc->getAuthorizeUrl(SHOPIFY_SCOPE, 'https://ec2staging.skylar.com/app');
