5d24871aa8c45a8031e4c94eabe771a984cc2dba
Tim Nolan <j.tim.nolan@gmail.com>
2020-07-16 17:46:01 -0700

    fix update edit quantity to work with 0

--- app/proxy/ajax/edit_quantity.php
+++ app/proxy/ajax/edit_quantity.php
@@ -15,17 +15,17 @@ if($quantity <= 0){
 		$stmt = $db->prepare("UPDATE rc_subscriptions SET deleted_at=? WHERE recharge_id=?");
 		$stmt->execute([
 			date('Y-m-d H:i:s'),
-			intval($_REQUEST['id']),
+			intval($_REQUEST['subscription_id']),
 		]);
 	} else {
-		$res = $this_res = $rc->post('/subscriptions/'.intval($_REQUEST['id']).'/cancel',[
+		$res = $this_res = $rc->post('/subscriptions/'.intval($_REQUEST['subscription_id']).'/cancel',[
 			'cancellation_reason' => $_REQUEST['reason'] ?? 'Item removed from customer account',
 			'send_email' => 'false',
 			'commit_update' => true,
 		]);
 		insert_update_rc_subscription($db, $this_res['subscription'], $rc, $sc);
 	}
-	log_event($db, 'SUBSCRIPTION', $_REQUEST['id'], 'CANCEL', 'Item removed from customer account', 'Cancelled via user account: '.json_encode($res), 'Customer');
+	log_event($db, 'SUBSCRIPTION', $_REQUEST['subscription_id'], 'CANCEL', 'Item removed from customer account', 'Cancelled via user account: '.json_encode($res), 'Customer');
 
 	if(!empty($res['error'])){
 		echo json_encode([
