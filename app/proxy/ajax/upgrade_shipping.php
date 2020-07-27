b7cd8238489e269156b2e8b89989e9dd73b11172
Tim Nolan <j.tim.nolan@gmail.com>
2020-07-27 12:44:58 -0700

    force charge refresh when updating shipping

--- app/proxy/ajax/upgrade_shipping.php
+++ app/proxy/ajax/upgrade_shipping.php
@@ -9,21 +9,21 @@ $new_shipping_lines = empty($_REQUEST['expedited']) ? null : [
 		'code' => 'US 2 Day',
 	]
 ];
-
-$res = $rc->put('addresses/'.$address_id, [
+$res_all = [];
+$res_all = $res = $rc->put('addresses/'.$address_id, [
 	'shipping_lines_override' => $new_shipping_lines,
 	'commit_update' => true,
 ]);
 $address = $res['address'];
 $charge = null;
 if(!empty($_REQUEST['charge_id'])){
-	$res = $rc->get('charges/'.intval($_REQUEST['charge_id']));
+	$res_all = $res = $rc->get('charges/'.intval($_REQUEST['charge_id']));
 	if(!empty($res['charge'])){
 		$ship_date_time = strtotime($res['charge']['scheduled_at']);
-		$res = $rc->post('charges/'.$res['charge']['id'].'/change_next_charge_date', [
+		$res_all = $res = $rc->post('charges/'.$res['charge']['id'].'/change_next_charge_date', [
 			'next_charge_date' => date('Y-m-d', strtotime('+1 day', $ship_date_time)),
 		]);
-		$res = $rc->post('charges/'.$res['charge']['id'].'/change_next_charge_date', [
+		$res_all = $res = $rc->post('charges/'.$res['charge']['id'].'/change_next_charge_date', [
 			'next_charge_date' => date('Y-m-d', $ship_date_time),
 		]);
 		$charge = $res['charge'];
@@ -38,6 +38,7 @@ if(!empty($res['error'])){
 		'new_shipping_lines' => $new_shipping_lines,
 		'success' => false,
 		'res' => $res,
+		'res_all' => $res_all,
 		'error' => $res['error']
 	]);
 } else {
@@ -46,5 +47,6 @@ if(!empty($res['error'])){
 		'charge' => $charge,
 		'success' => true,
 		'res' => $res,
+		'res_all' => $res_all,
 	]);
 }
